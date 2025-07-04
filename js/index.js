f715b8551b1c909ce6154e299fb3c8b04ab88789
sergii.ignatov <s.ignatov92@gmail.com>
2025-07-04 15:48:38 +0300

    continuing

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-console.log('Happy developing ✨')
+let myLeads = [];
+const inputBtn = document.getElementById('input-btn');
+const inputEl = document.getElementById('input-el');
+const ulEl = document.getElementById('ul-el');
+
+inputBtn.addEventListener('click', function(){
+    myLeads.push(inputEl.value);
+    addListElement();
+    // console.log(myLeads);
+});
+
+function addListElement(){
+    let liEl = document.createElement('li');
+    liEl.textContent = inputEl.value;
+    ulEl.appendChild(liEl);
+}
+
