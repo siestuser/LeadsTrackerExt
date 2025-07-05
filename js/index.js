8d1bc8b8d5d33255b614207703d2939cf29648a5
sergii.ignatov <s.ignatov92@gmail.com>
2025-07-05 13:26:14 +0300

    added more styling, error handling and refactored code

--- js/index.js
+++ js/index.js
@@ -2,16 +2,30 @@ let myLeads = [];
 const inputBtn = document.getElementById('input-btn');
 const inputEl = document.getElementById('input-el');
 const ulEl = document.getElementById('ul-el');
+const errorDiv = document.getElementById('error-div');
 
 inputBtn.addEventListener('click', function(){
-    myLeads.push(inputEl.value);
     addListElement();
-    // console.log(myLeads);
 });
 
 function addListElement(){
-    let liEl = document.createElement('li');
-    liEl.textContent = inputEl.value;
-    ulEl.appendChild(liEl);
+    if(inputEl.value !== ''){
+        //hide error
+        errorDiv.style.display = 'none';
+        //add to array, display item and clear the field
+        myLeads.push(inputEl.value);
+        //create list element;
+        let liEl = document.createElement('li');
+        liEl.innerHTML = `<a href="${inputEl.value}" target="_blank">${inputEl.value}</a>`;
+        ulEl.appendChild(liEl);
+        clearField();
+    } else {
+        //show error
+        errorDiv.style.display = 'block';
+    }
+}
+
+function clearField(){
+    inputEl.value = '';
 }
 
