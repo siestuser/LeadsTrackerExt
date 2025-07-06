c7b67abbbbb9af824be677fd989cc84b14bad8b2
sergii.ignatov <s.ignatov92@gmail.com>
2025-07-06 13:49:21 +0300

    added Save Tab URL functionality refactored code

--- js/index.js
+++ js/index.js
@@ -1,39 +1,55 @@
 let myLeads = [];
+const localStorageKey = 'myLeads';
 const inputBtn = document.getElementById('input-btn');
+const tabBtn = document.getElementById('tab-btn');
 const clearBtn = document.getElementById('clear-btn');
 const inputEl = document.getElementById('input-el');
 const ulEl = document.getElementById('ul-el');
 const errorDiv = document.getElementById('error-div');
 
 inputBtn.addEventListener('click', function(){
-    addListElement();
+    saveInputElement(inputEl.value, myLeads, localStorageKey, ulEl);
 });
 
-window.onload = function(){
-    listLeadsFromLocalStorage();
-}
+tabBtn.addEventListener('click', function(){
+    saveCurrentActiveTab(myLeads, localStorageKey, ulEl);
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+    renderListFromStorage(localStorageKey, ulEl);
+});
 
 clearBtn.addEventListener('click', function(){
     clearLeads();
-})
-function addListElement(){
-    if(inputEl.value !== ''){
+});
+
+//function to get active Tab URL
+function saveCurrentActiveTab(array, storageKey, listObject) {
+    let activeTabURL;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        activeTabURL = tabs[0].url;
+    });
+    array = getFromLocalStorage(storageKey) || [];
+    array.push(activeTabURL);
+    renderLiElement(activeTabURL, listObject);
+    saveToLocalStorage(storageKey, array);
+}
+
+//save the input element to local storage
+function saveInputElement(inputValue, array, storageKey, listObject){
+    if(inputValue !== ''){
         //hide error
         errorDiv.style.display = 'none';
         //add to array, display item and clear the field
-        if(!myLeads){
-            myLeads.push(inputEl.value);
-        }else{
-            myLeads = JSON.parse(localStorage.getItem('myLeads')) || [];
-            myLeads.push(inputEl.value);
-        }
-        console.log(myLeads);
-        // myLeads.push(inputEl.value);
-        //create list element;
-        let liEl = document.createElement('li');
-        liEl.innerHTML = `<a href="${inputEl.value}" target="_blank">${inputEl.value}</a>`;
-        ulEl.appendChild(liEl);
-        localStorage.setItem('myLeads', JSON.stringify(myLeads));
+        array = getFromLocalStorage(storageKey) || [];
+        array.push(inputValue);
+        //create HTML list element;
+        renderLiElement(inputValue,listObject);
+
+        //save item to local storage
+        saveToLocalStorage(storageKey, array);
+
+        //clear input field;
         inputEl.value = '';
     } else {
         //show error
@@ -41,15 +57,29 @@ function addListElement(){
     }
 }
 
+//saves the array of strings (leads) to Local storage
+function saveToLocalStorage(key, array){
+    localStorage.setItem(key, JSON.stringify(array));
+}
 
+//returns array of leads from Local Storage
+function getFromLocalStorage(key){
+    return JSON.parse(localStorage.getItem(key));
+}
 
-function listLeadsFromLocalStorage(){
-    let leads = JSON.parse(localStorage.getItem('myLeads'));
+//add item to the HTML list
+function renderLiElement(listItem, listObject){
+    let liEl = document.createElement('li');
+    liEl.innerHTML = `<a href="${listItem}" target="_blank">${listItem}</a>`;
+    listObject.appendChild(liEl);
+}
+
+//load and render leads from local storage
+function renderListFromStorage(storedLeads, listObject){
+    let leads = getFromLocalStorage(storedLeads);
 
     for (let lead of leads){
-        let liEl = document.createElement('li');
-        liEl.innerHTML = `<a href="${lead}" target="_blank">${lead}</a>`;
-        ulEl.appendChild(liEl);
+        renderLiElement(lead, listObject);
     }
 
 }
