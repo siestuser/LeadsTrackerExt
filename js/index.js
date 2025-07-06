975a100d1d12303cdafce9516da93ea1c4f9ee28
sergii.ignatov <s.ignatov92@gmail.com>
2025-07-06 12:17:36 +0300

    fixed bug with local storage being cleared after refresh

--- js/index.js
+++ js/index.js
@@ -21,7 +21,14 @@ function addListElement(){
         //hide error
         errorDiv.style.display = 'none';
         //add to array, display item and clear the field
-        myLeads.push(inputEl.value);
+        if(!myLeads){
+            myLeads.push(inputEl.value);
+        }else{
+            myLeads = JSON.parse(localStorage.getItem('myLeads')) || [];
+            myLeads.push(inputEl.value);
+        }
+        console.log(myLeads);
+        // myLeads.push(inputEl.value);
         //create list element;
         let liEl = document.createElement('li');
         liEl.innerHTML = `<a href="${inputEl.value}" target="_blank">${inputEl.value}</a>`;
@@ -38,11 +45,13 @@ function addListElement(){
 
 function listLeadsFromLocalStorage(){
     let leads = JSON.parse(localStorage.getItem('myLeads'));
+
     for (let lead of leads){
         let liEl = document.createElement('li');
         liEl.innerHTML = `<a href="${lead}" target="_blank">${lead}</a>`;
         ulEl.appendChild(liEl);
     }
+
 }
 
 //clearing things
